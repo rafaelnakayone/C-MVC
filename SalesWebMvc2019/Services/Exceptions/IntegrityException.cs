b268cbaa1d7afc8a5e87f0363b5acf0afcc1cb69
Rafael Nakayone <rafael_nakayone@hotmail.com>
2023-04-23 14:03:20 -0300

    Exception handling for delete (referential integrity)

---
+++ SalesWebMvc2019/Services/Exceptions/IntegrityException.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+
+namespace SalesWebMvc2019.Services.Exceptions
+{
+    public class IntegrityException : ApplicationException
+    {
+        public IntegrityException(string message) : base(message)
+        {
+
+        }
+
+    }
+}
