f25ce4a9b8681f82df0e84316051c75d1017ebb1
Rafael Nakayone <rafael_nakayone@hotmail.com>
2023-04-23 13:53:08 -0300

    Asynchronous operations using Task (async, await)

--- SalesWebMvc2019/Services/DepartmentService.cs
+++ SalesWebMvc2019/Services/DepartmentService.cs
@@ -2,6 +2,8 @@
 using SalesWebMvc2019.Models.SalesWebMvc;
 using System.Collections.Generic;
 using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
 
 namespace SalesWebMvc2019.Services
 {
@@ -14,9 +16,10 @@ namespace SalesWebMvc2019.Services
             _context = context;
         }
 
-        public List<Department> FindAll()
+        public async Task<List<Department>> FindAllAsync() /*Quando a aplicação é sincrona, quando vai acessar dados, disco ou rede, a operação fica muito lenta.
+                                                       * Para isso utilizamos a aplicação assincrona para ficar mais rápido*/
         {
-            return _context.Department.OrderBy(x => x.Name).ToList();
+            return await _context.Department.OrderBy(x => x.Name).ToListAsync();
 
         }
     }
