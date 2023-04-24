234fd474a13ef4833bff345a6fe2ea417bd9d9ed
Rafael Nakayone <rafael_nakayone@hotmail.com>
2023-04-24 15:54:57 -0300

    Final Version

---
+++ SalesWebMvc2019/Migrations/20230424185030_UpdatingSells.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace SalesWebMvc2019.Migrations
+{
+    public partial class UpdatingSells : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+
+        }
+    }
+}
